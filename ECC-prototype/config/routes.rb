e4a8244bb9b1a244474aabce28d992357011671c
jake-freeman <jfreeman@hawk.iit.edu>
2016-09-20 14:41:27 -0500

    Add initial pages based on front-end team

--- ECC-prototype/config/routes.rb
+++ ECC-prototype/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # default page
+  root 'static#main'
+
+  # static pages
+  get '/:page' => 'static#show'
 end
