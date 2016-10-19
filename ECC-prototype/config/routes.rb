7e442affe365798726c6a128b3a9db1f84e2ed2a
jake-freeman <jfreeman@hawk.iit.edu>
2016-10-19 16:45:52 -0500

    Integrate Camaleon CMS with default templace and config

--- ECC-prototype/config/routes.rb
+++ ECC-prototype/config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # default page
-  root 'static#main'
+  #root 'static#main'
 
   # static pages
-  get '/:page' => 'static#show'
+  #get '/:page' => 'static#show'
 end
